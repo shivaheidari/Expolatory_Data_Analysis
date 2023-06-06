ce1ddcbd927c5b86492369aee384e4189f6ff7da
Shiva Heydari <shivaheydari.es@gmail.com>
2023-06-06 13:23:04 -0600

    Adding fisher

--- Data/Tests.py
+++ Data/Tests.py
@@ -17,23 +17,31 @@ class file_reader:
 
         with open("setting.json") as file:
             setting = json.load(file)
-
         self.data_type = setting["data_type"]
         self.confidence = setting["confidance"]
         self.source = setting["variable_source"]
         self.target = setting["variable_target"]
     def create_test(self):
-        if self.data_type == "categorical":
-            for s in self.source:
-                for t in self.target:
-                    obj = chi_square(self.df[s], self.df[t], self.confidence)
-                    obj.significance()
-                    obj.print_to_file()
+        # if self.data_type == "categorical":
+        #     for s in self.source:
+        #         for t in self.target:
+        #             obj = chi_square(self.df[s], self.df[t], self.confidence)
+        #             obj.significance()
+        #             self.result = obj.get_total()
+        #             file = open("res_export.txt", 'w+')
+        #             file.write(s + " and " + t+" "+ self.result["result"]+"\n")
+        #             file.write(str(self.result))
 
 
-
-
-
+            if self.data_type == "categorical":
+                for s in self.source:
+                    for t in self.target:
+                        obj = fisher_test(self.df[s], self.df[t], self.confidence)
+                        self.result = obj.significance()
+                        # self.result = obj.get_total()
+                        file = open("res_fisher.txt", 'w+')
+                        file.write(s + " and " + t+" "+ self.result["result"]+"\n")
+                        file.write(str(self.result))
 class chi_square:
 
     def __init__(self, var1, var2, prob):
@@ -48,34 +56,51 @@ class chi_square:
         contigency_matrix = pd.crosstab(self.var1, self.var2)
         self.contigency_matrix = contigency_matrix
         chi_stat, p_val, dof, expected = stats.chi2_contingency(contigency_matrix)
-        self.dict_vals["chi_stat"] = chi_stat
-        self.dict_vals["p_val"] = p_val
-        self.dict_vals["dof"] = dof
-        self.dict_vals["expected"] = expected
         critical = stats.chi2.ppf(self.prob, dof)
         if abs(chi_stat) >= critical:
             self.res = "reject H0"
 
         else:
             self.res = "Fail to reject"
-        return self.res
+
+        self.dict_vals["chi_stat"] = chi_stat
+        self.dict_vals["p_val"] = p_val
+        self.dict_vals["dof"] = dof
+        self.dict_vals["expected"] = expected
+        self.dict_vals["result"] = self.res
 
     def get_contigency_matrix(self):
         return self.contigency_matrix
 
     def get_total(self):
         return self.dict_vals
-    def print_to_file(self):
-        file = open("chi_export.txt", 'w+')
-        file.write(str(self.var1)+"and"+str(self.var2)+self.res)
-        file.write(str(self.dict_vals)+"\n"+str(self.contigency_matrix))
-        file.close()
 
 
 
+class fisher_test:
+    def __init__(self, var1, var2, prob):
+        self.var1 = var1
+        self.var2 = var2
+        self.alpha = 1-prob
+        self.result = {}
+    def significance(self):
+        self.contingency_matirx = pd.crosstab(self.var1, self.var2)
+        if (self.contingency_matirx.shape == (2,2)):
+            odds_ratio, p_value = stats.fisher_exact(self.contingency_matirx)
+            print("odds Ration", odds_ratio)
+            print("p-value", p_value)
 
+            if p_value <= (1-self.alpha):
+                res =  "Reject H0"
+            else:
+                res = "fail to reject"
+            self.result["result"] = res
+            self.result["p_value"] = p_value
+            self.result["odds_ratio"] = odds_ratio
+            return self.result
 obj = file_reader()
 obj.create_test()
+# obj.print_to_file()
 # data = pd.read_csv("dt_test.csv")
 # df = pd.DataFrame(data)
 # ob = chi_square(df["v1"], df["v2"], 0.95)
